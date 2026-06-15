f9a30922e90362100ec2fe60776d1ad75e4a95d7
zlfkraffandi <zlfkraffandi@github.com>
2026-06-15 15:02:56 +0700

    Add PWA support: manifest, service worker, icons

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'povi-expense-v1'
+const STATIC = ['/', '/index.html']
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)))
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ))
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('/api/')) return
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  )
+})
